1a113edc3be1f40f3a100ea08dc5e9c93639024d
Daniel_Fernandes <danielgiovane2015@gmail.com>
2019-12-18 08:43:13 -0300

    iniciando com react 0.0.1

--- main-react.js
+++ main-react.js
@@ -1,12 +1,39 @@
-const ProdutoComponent = () => {
+const produtosListas = [
+    {
+        id: 'bbc123',
+        nome: 'JsRaiz para Node',
+        preco: 1200,
+        descricao: 'melhor curso',
+        img: './img/kimetsu-no-yaiba-infinite-train-hashira_eeye.png'
+    },
+
+    {
+        id: 'cbc123',
+        nome: 'Programção funcional',
+        preco: 500,
+        descricao: 'melhor curso',
+        img: './img/kimetsu-no-yaiba-infinite-train-hashira_eeye.png'
+    },
+
+    {
+        id: 'dbc123',
+        nome: 'JsRaiz para inesperientes',
+        preco: 300,
+        descricao: 'melhor curso',
+        img: './img/kimetsu-no-yaiba-infinite-train-hashira_eeye.png'
+    },
+]
+
+
+const ProdutoComponent = (produto) => {
     return (
         React.createElement('div', { className: 'col-sm-4 mb-3' },
             React.createElement('div', { className: 'card loja_item' },
-                React.createElement('img', { className: 'card img-top', src: "./img/kimetsu-no-yaiba-infinite-train-hashira_eeye.png", height: "180px" }),
+                React.createElement('img', { className: 'card img-top', src: produto.img, height: "180px" }),
                 React.createElement('div', { className: 'card-body' },
-                    React.createElement('h5', { className: 'card-title' }, 'JsRaiz para Frameworks'),
-                    React.createElement('small', null, 'R$300,00'),
-                    React.createElement('p', { className: 'card-text' }, 'melhor curso'),
+                    React.createElement('h5', { className: 'card-title' }, produto.nome),
+                    React.createElement('small', null, `R$${produto.preco}`),
+                    React.createElement('p', { className: 'card-text' }, produto.descricao),
                     React.createElement('button', { className: 'btn btn-primary btn-add' }, 'Adicionar')
                 )
             )
@@ -15,12 +42,11 @@ const ProdutoComponent = () => {
 }
 
 
-const ListaProdutoComponent = () => {
+const ListaProdutoComponent = ({itens}) => {
+    console.log(itens)
     return (
         React.createElement('div', { className: 'row loja' },
-            React.createElement(ProdutoComponent),
-            React.createElement(ProdutoComponent),
-            React.createElement(ProdutoComponent)
+         itens.map((produto,ind) => React.createElement(ProdutoComponent,produto))
         )
     )
 }
@@ -28,7 +54,7 @@ const ListaProdutoComponent = () => {
 const carrinhoItensComponent = () => {
     return (
         React.createElement('div', { className: 'carrinho' },
-            React.createElement('div', { className: 'carrinho_itens' },
+            React.createElement('div', { className: 'card carrinho_itens' },
                 React.createElement('div', { className: 'carrinho_item' },
                     React.createElement('div', { className: 'card-body' },
                         React.createElement('h5', { className: 'card-title' }, 'JsRaiz para fw'),
@@ -54,7 +80,7 @@ function AppComponent() {
     return (
         React.createElement(React.Fragment, null,
             React.createElement('div', { className: 'col-sm-8' },
-                React.createElement(ListaProdutoComponent)
+                React.createElement(ListaProdutoComponent, {itens: produtosListas})
             ),
             React.createElement('div', { className: 'col-sm-4' },
                 React.createElement(carrinhoItensComponent),
